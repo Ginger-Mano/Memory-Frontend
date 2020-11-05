cc34f19d8784e1effa5e47135c0c93f49808596c
kelseyjdickson <kelseymedved@gmail.com>
2020-11-05 12:11:36 -0500

    fetched successful

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
 import React from 'react';
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+class App extends React.Component{
+
+// declare a new state variable 
+// const [cards, setCards] =  useState([])
+
+state = {
+  cards: []
+}
+
+componentDidMount(){
+  fetch('http://localhost:3000/cards')
+    .then(res => res.json())
+     .then(res => console.log('made it!', res))
+}
+
+// useEffect(() => {
+//   fetch('http://localhost:3000/cards')
+//   .then(res => res.json())
+//   .then(res => console.log('made it!', cards))
+// },[])
+
+render(){
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+       
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This is App.js
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    
       </header>
     </div>
-  );
+);
 }
-
-export default App;
+}
+export default App
