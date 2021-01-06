41a50c81240baf9dc5248ad96be8fc230579db19
kelseyjdickson <kelseymedved@gmail.com>
2021-01-06 12:07:55 -0500

    added component Home and moved children into

--- src/App.js
+++ src/App.js
@@ -3,78 +3,87 @@ import AppWithHooks from './AppWithHooks'
 import NavBar from './Navbar'
 import {Route, Switch} from 'react-router-dom'
 import './App.css';
-import CardContainerFront from './CardContainerFront';
-import ReactCardFlip from 'react-card-flip';
-import CardContainerBack from './CardContainerBack';
-import Dashboard from './Dashboard';
+import Home from './Home'
 import About from './About';
+
+// -------COPIED TO HOME ----------
+// import CardContainerFront from './CardContainerFront';
+// import ReactCardFlip from 'react-card-flip';
+// import CardContainerBack from './CardContainerBack';
+// import Dashboard from './Dashboard';
 
 
 class App extends React.Component {
 
-  // declare a new state variable 
-  // const [cards, setCards] =  useState([])
+//  -----COPIED TO HOME -----
 
-  state = {
-    cards: [],
-    isFlipped: false,
-    users: []
-  }
+  // state = {
+  //   cards: [],
+  //   isFlipped: false,
+  //   users: []
+  // }
 
-  componentDidMount() {
-    fetch('http://localhost:3000/cards')
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/cards')
 
-    .then(res => res.json())
-    .then(data =>
-      this.setState({
-        cards: this.suffer(data)
+  //   .then(res => res.json())
+  //   .then(data =>
+  //     this.setState({
+  //       cards: this.suffer(data)
         
-      }))
-      fetch('http://localhost:3000/users')
+  //     }))
+  //     fetch('http://localhost:3000/users')
       
-      .then(res => res.json())
-      .then(data =>
-        this.setState({
-          users: data
-        })
-        )
-      }
+  //     .then(res => res.json())
+  //     .then(data =>
+  //       this.setState({
+  //         users: data
+  //       })
+  //       )
+  //     }
       
+      //  ------- END of copied to home---
       
-      
-      suffer = (cards) => {
-       for(let i = 0; i > cards.length; i++){
-         let randomIdx = Math.floor(Math.random() * cards.length)
-         let copyCurrent = {...cards[i]}
-         let copyRandom = {...cards[randomIdx]}
-         cards[i] = copyRandom
-         cards[randomIdx] = copyCurrent
-       }
-       return cards
-     }
+    //   suffer = (cards) => {
+    //    for(let i = 0; i > cards.length; i++){
+    //      let randomIdx = Math.floor(Math.random() * cards.length)
+    //      let copyCurrent = {...cards[i]}
+    //      let copyRandom = {...cards[randomIdx]}
+    //      cards[i] = copyRandom
+    //      cards[randomIdx] = copyCurrent
+    //    }
+    //    return cards
+    //  }
 
 
 
 
 
   render() {
-    // let randomNum = this.state.cards.
-    let front = this.state.cards.map((card) => {
-      return <CardContainerFront key={card.id} card={card} randomID={this.randomID} isFlipped={this.state.isFlipped} />
-    })
-    let userInfo = this.state.users.map((user) => {
-      return <Dashboard key={user.id} user={user} />
-    })
+    
+
+
+// -------COPIED TO HOME---------
+    // let front = this.state.cards.map((card) => {
+    //   return <CardContainerFront key={card.id} card={card} randomID={this.randomID} isFlipped={this.state.isFlipped} />
+    // })
+    // let userInfo = this.state.users.map((user) => {
+    //   return <Dashboard key={user.id} user={user} />
+    // })
 
     return (
       <div className="App">
-        {/* <Switch>
-        <Route path="/about" render={ () => <About/>}/>
-        </Switch> */}
-        <Route path="/">
+        
         <NavBar />
+        <Switch>
+         <Route exact path="/" render={ ()=> <Home /> }></Route>
+         <Route path="/about" render={ ()=> <About /> }></Route>
+         
+          
+        </Switch>
 
-        <div className="most-outer-div">
+{/* COPIED TO HOME */}
+        {/* <div className="most-outer-div">
           <div className="second-div">
             {front}
           </div>
@@ -82,10 +91,10 @@ class App extends React.Component {
             {userInfo}
           </div>
         </div>
-        </Route>
+       
+      </div> */}
       </div>
-
-    );
+)
   }
 }
 export default App
---
+++ src/Home.js
@@ -0,0 +1,80 @@
+import React from 'react'
+import CardContainerFront from './CardContainerFront';
+import ReactCardFlip from 'react-card-flip';
+import CardContainerBack from './CardContainerBack';
+import Dashboard from './Dashboard';
+import About from './About';
+
+
+
+
+class Home extends React.Component {
+
+
+    state = {
+        cards: [],
+        isFlipped: false,
+        users: []
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:3000/cards')
+
+            .then(res => res.json())
+            .then(data =>
+                this.setState({
+                    cards: this.suffer(data)
+
+                }))
+        fetch('http://localhost:3000/users')
+
+            .then(res => res.json())
+            .then(data =>
+                this.setState({
+                    users: data
+                })
+            )
+    }
+    suffer = (cards) => {
+        for(let i = 0; i > cards.length; i++){
+          let randomIdx = Math.floor(Math.random() * cards.length)
+          let copyCurrent = {...cards[i]}
+          let copyRandom = {...cards[randomIdx]}
+          cards[i] = copyRandom
+          cards[randomIdx] = copyCurrent
+        }
+        return cards
+      }
+
+
+
+    render() {
+        let front = this.state.cards.map((card) => {
+            return <CardContainerFront key={card.id} card={card} randomID={this.randomID} isFlipped={this.state.isFlipped} />
+        })
+        let userInfo = this.state.users.map((user) => {
+            return <Dashboard key={user.id} user={user} />
+        })
+
+        return (
+            <div>
+                <h1>Home</h1>
+
+                <div className="most-outer-div">
+                    <div className="second-div">
+                        {front}
+                    </div>
+                    <div className="div-dash">
+                        {userInfo}
+                    </div>
+                </div>
+
+            </div>
+        )
+    }
+
+}
+
+
+
+export default Home
